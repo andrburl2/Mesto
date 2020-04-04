344d36aa724087a65ab79965e9948a16280de959
Andrey Burlakov <andrburl2@yandex.ru>
2020-04-04 22:54:15 +0300

    Добавил логирование

--- routes/index.js
+++ routes/index.js
@@ -9,18 +9,20 @@ const error = require('./error');
 const auth = require('../middlewares/auth');
 const errorHandler = require('../middlewares/errorHandler');
 const { validateRegistration, validateLogin } = require('../assets/joi-schemes');
+const { requestLogger, errorLogger } = require('../middlewares/logger');
+
+router.use(requestLogger);
 
 router.post('/signup', validateRegistration, createUser);
 router.post('/signin', validateLogin, login);
 
 router.use(auth);
-
 router.use('/cards', cards);
 router.use('/users', users);
+
+router.use(errorLogger);
 router.use(error);
-
 router.use(errors());
-
 router.use(errorHandler);
 
 module.exports = router;
